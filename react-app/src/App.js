2993f2108b2a6e9774297100f99dc8a44994db2e
zaheer <zaheerme-211@hotmail.com>
2018-03-12 00:55:55 +0500

    SideNav in progress

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import SideNav from './components/SideNav';
-import './App.css';
 
 class App extends Component {
   render() {
--- react-app/src/components/SideNav.js
+++ react-app/src/components/SideNav.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+import './SideNav.css';
 
 export default class SideNav extends Component {
+
   render() {
+
     return (
       <div className="SideNav">
-        Side Nav
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='overlay' width='thin' visible={true} icon='labeled' vertical>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item name='block layout'>
+              <Icon name='block layout' />
+              Products
+            </Menu.Item>
+            <Menu.Item name='settings'>
+              <Icon name='settings' />
+              Settings
+            </Menu.Item>
+            <Menu.Item name='log out'>
+              <Icon name='log out' />
+              Logout
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher className="content">
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              {/* <Image src='/assets/images/wireframe/paragraph.png' /> */}
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     );
   }
